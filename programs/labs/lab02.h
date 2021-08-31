7b35aebf28e50bc5adccd3b6fb411bf3562b26f4
Rodrigo Vianna <rodrigoignaciovianna@gmail.com>
2021-08-31 08:13:08 -0300

    correções código

--- programs/labs/lab02.h
+++ programs/labs/lab02.h
@@ -7,8 +7,7 @@ DigitalOut led_azul(LED_BLUE_L,false);
 DigitalOut led_verde_L(LED_GREEN_L,!false);
 DigitalOut led_verde_R(LED_GREEN_R,!false);
 // Define all motors as PWM objects
-PwmOut motor(MOTOR1);
-motor.period(1.0/500.0);
+PwmOut motor1(MOTOR1);
 
 
 float control_motor(float omega_r) {
@@ -18,10 +17,12 @@ float control_motor(float omega_r) {
 int main()
 {
     //turn on motor 1 with 1000 rad/s for 0.5s
-    motor= control_motor(1000);
+    motor1= control_motor(1000);
+    motor1.period(1.0/500.0);
+
     wait(0.5);
     //Turn off motor 1
-    motor=0.0;
+    motor1=0.0;
 
     while(true)
     {
